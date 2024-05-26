b79bacfee11eb0f5fcabadaa5946a9c1752288c3
Lee <leedunham@gmail.com>
2024-05-26 22:02:48 +0200

    README/init updated

--- abstract_factories/__init__.py
+++ abstract_factories/__init__.py
@@ -1,10 +1,10 @@
 """
 `abstract_factories` provides functional factory classes extending the
 conventional Abstract Factory design pattern.
+The primary use case is to provide a clear layer of abstraction
+for scalable data.
 
-Influenced by;
-- https://github.com/mikemalinowski/factories
-
+Abstract factories is influenced by https://github.com/mikemalinowski/factories.
 """
 from .constants import LOGGER
 
