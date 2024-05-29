9874289ca1e30d0e57b919e0d4cd64428a71bbda
Lee <leedunham@gmail.com>
2024-05-29 23:12:58 +0200

    long_description setup readded

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
 setup(
     name='abstract_factories',
     packages=['abstract_factories'],
     version='0.2.3',
     license='MIT',
     description='Abstract Factory design pattern classes for scalable data in dynamic environments.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Lee Dunham',
     author_email='leedunham@gmail.com',
     url='https://github.com/ldunham1/abstract_factories',
