1443fc9396a5b9a4068b9cc268f5843725578a87
Lee <leedunham@gmail.com>
2024-05-29 22:04:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 from setuptools import setup
 
 
+with open("README.txt") as f:
+    long_description = f.read()
+
+
 setup(
     name='abstract_factories',
     packages=['abstract_factories'],
     version='0.2.2',
     license='MIT',
     description='Abstract Factory design pattern classes for scalable data in dynamic environments.',
+    long_description=long_description,
     author='Lee Dunham',
     author_email='leedunham@gmail.com',
     url='https://github.com/ldunham1/abstract_factories',
     classifiers=[
-        'Programming Language :: Python :: 3',
+        "Intended Audience :: Developers",
         'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
     ],
 )
