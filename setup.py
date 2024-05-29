5ff8513512def595c74d5f859a9c95a384c99c37
Lee <leedunham@gmail.com>
2024-05-29 23:15:33 +0200

    Update setup.py version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='abstract_factories',
     packages=['abstract_factories'],
-    version='0.2.3',
+    version='0.2.4',
     license='MIT',
     description='Abstract Factory design pattern classes for scalable data in dynamic environments.',
     long_description=long_description,
