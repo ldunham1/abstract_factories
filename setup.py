d9e450bcccfbf8fc2ec6632d1282f74c4ea68434
Lee <leedunham@gmail.com>
2024-05-29 23:40:43 +0200

    Removed github-release from workflow
    Obsolete considering workflow is triggered by the release tag

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='abstract_factories',
     packages=['abstract_factories'],
-    version='0.2.4',
+    version='0.2.41',
     license='MIT',
     description='Abstract Factory design pattern classes for scalable data in dynamic environments.',
     long_description=long_description,
