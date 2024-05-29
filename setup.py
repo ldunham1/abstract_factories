f7eecd25fe8b28ad5f598f5e0d5d79abbd7cc268
Lee <leedunham@gmail.com>
2024-05-29 22:57:00 +0200

    Setup removed long_description
    rst formatting issues irrelevant to release.

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup
 
 
-with open("README.md") as f:
-    long_description = f.read()
-
-
 setup(
     name='abstract_factories',
     packages=['abstract_factories'],
     version='0.2.3',
     license='MIT',
     description='Abstract Factory design pattern classes for scalable data in dynamic environments.',
-    long_description=long_description,
     author='Lee Dunham',
     author_email='leedunham@gmail.com',
     url='https://github.com/ldunham1/abstract_factories',
