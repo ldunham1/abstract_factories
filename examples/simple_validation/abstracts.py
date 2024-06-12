55abd1a3c51a110ebe4831c1c137706bc09b8866
Lee Dunham <leedunham@gmail.com>
2024-06-12 23:54:53 +0200

    Update simple_validator abstracts.py

--- examples/simple_validation/abstracts.py
+++ examples/simple_validation/abstracts.py
@@ -2,6 +2,11 @@ import types
 
 
 class Context(object):
+    """
+    Represents a piece of data to validate.
+    Data is found and wrapped as Contexts by Collectors.
+    These Contexts are then passed to Validators.
+    """
 
     def __init__(self, label, data):
         self.label = label
@@ -12,6 +17,10 @@ class Context(object):
 
 
 class AbstractCollector(object):
+    """
+    A Collector is responsible for finding and wrapping data into individual Contexts.
+    Typically, the data is given as a Dictionary as it's mutable and easily queried.
+    """
 
     def collect(self):
         # type: () -> types.GeneratorType[Context]
@@ -19,6 +28,11 @@ class AbstractCollector(object):
 
 
 class AbstractValidator(object):
+    """
+    A Validator will validate the data in a Context.
+    It can return a list of any issues found.
+    As Context data is mutable, if an autofix can be applied, the data can be modified before continuing to the next Validator.
+    """
 
     def __str__(self):
         return '{}()'.format(self.__class__.__name__)
