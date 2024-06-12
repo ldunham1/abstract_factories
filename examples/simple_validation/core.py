20610108adbbd78a594a34a6dcc0293e0a292be4
Lee Dunham <leedunham@gmail.com>
2024-06-13 00:06:54 +0200

    Update simple_validation core.py

--- examples/simple_validation/core.py
+++ examples/simple_validation/core.py
@@ -6,12 +6,25 @@ from . import abstracts, json_checker
 
 
 class DataValidator(object):
+    """
+    Simple Validation example to find and validate data.
+    Multiple abstract factories are used (and could be extended on) in order to provide the convenience desired;
+    - Finding the data to validate.
+    - Validating the data.
+    - (Optional) Do something else with the validated data.
+
+    AbstractInstanceFactories are used here as we don't need to create multiple instances of types. We're only interested in the instance (allowing for a singular type to create multiple registered instances that have different behaviours - ie collecting different file types or from different paths).
+    """
 
     def __init__(self):
         self.collector_factory = AbstractInstanceFactory(abstracts.AbstractCollector, modules=[json_checker])
         self.validator_factory = AbstractInstanceFactory(abstracts.AbstractValidator, modules=[json_checker])
 
     def collect(self):
+        """
+        Get Contexts found by registered Collectors.
+        :rtype: list[Context]
+        """
         return [
             context
             for collector in self.collector_factory.items()
@@ -19,11 +32,20 @@ class DataValidator(object):
         ]
 
     def validate(self, context_list=None):
+        """
+        Validate Contexts using registered Validators.
+        Any validation issues are added to a dictionary identified by Context and Validator.
+        :rtype: dict[str, dict[str, list[str]]]
+        """
         results = defaultdict(dict)
+
+        # Use either given contexts or all available.
         context_list = context_list or self.collect()
         for context in context_list:
             for validator in self.validator_factory.items():
                 issues = validator.validate(context)
                 if issues:
                     results[str(context)][str(validator)] = issues
-        return results
+
+        # return a native dict type
+        return dict(results)
