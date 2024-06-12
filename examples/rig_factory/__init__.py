deed786a4a495f1c2e3df5c58da7111fae7217a7
Lee Dunham <leedunham@gmail.com>
2024-06-12 11:19:58 +0200

    Update rig_factory __init__ linebreaks

--- examples/rig_factory/__init__.py
+++ examples/rig_factory/__init__.py
@@ -1,13 +1,21 @@
 """
-This example uses `abstract_factories` to find, register and manage AbstractRigComponent subclasses.
+This example uses `abstract_factories` to find, register and manage 
+AbstractRigComponent subclasses.
 
-A typical issue with rig component management is reliability managing rig component versions, as latest versions should be used by default but earlier versions should still be accessible when needed.
+A typical issue with rig component management is reliability managing 
+rig component versions, as latest versions should be used by default but 
+earlier versions should still be accessible when needed.
 
 This is made easy with the Type Factory.
-Intentionally grouping AbstractRigComponent 
-subclasses by class name promotes the good practice of ensuring component versions are not from the same module (reduces risk of accidental modification and easier to debug) and then sorts by their Version identifier.
-The `factory.get()` method will return the latest versioned item, unless a version value is given.
+Intentionally grouping AbstractRigComponent subclasses by class name 
+promotes the good practice of ensuring component versions are not from the 
+same module (reduces risk of accidental modification and easier to debug) and 
+then sorts by their Version identifier.
+The `factory.get()` method will return the latest versioned item, unless a 
+version value is given.
 
-Unlike traditional Factory and Abstract Factory designs, interactions with the items directly is encouraged, but ensure the abstract itself provides all expected abstract methods and parameters.
+Unlike traditional Factory and Abstract Factory designs, interactions with 
+the items directly is encouraged, but ensure the abstract itself provides all 
+expected abstract methods and parameters.
 """
 from .core import RigComponentBuilder
